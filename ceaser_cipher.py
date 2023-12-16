e40003127f779098bc44d099207143be781f07d6
Abi <abi.sivasith.world@gmail.com>
2023-12-16 20:10:06 +1300

    created ceaser_cipher function (incomplete)

--- ceaser_cipher.py
+++ ceaser_cipher.py
@@ -4,3 +4,37 @@ function to encode the provided text using the ceaser cipher technique
 by Abilash Sivasith
 
 """
+
+ALPABET_MAPPING = {
+    'a': 0,
+    'b': 1,
+    'c': 2,
+    'd': 3,
+    'e': 4,
+    'f': 5,
+    'g': 6,
+    'h': 7,
+    'i': 8,
+    'j': 9,
+    'k': 10,
+    'l': 11,
+    'm': 12,
+    'n': 13,
+    'o': 14,
+    'p': 15,
+    'q': 16,
+    'r': 17,
+    's': 18,
+    't': 19,
+    'u': 20,
+    'v': 21,
+    'w': 22,
+    'x': 23,
+    'y': 24,
+    'z': 25
+}
+
+def ceaser_cipher(plaintext, shift):
+    """encrypts provided sentance"""
+    pass
+
