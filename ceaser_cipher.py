0909a29c7bae264449fea7215d30ead0af34fb73
Abi <abi.sivasith.world@gmail.com>
2023-12-18 16:13:30 +1300

    ceaser_cipher_decrypter function created and tested

--- ceaser_cipher.py
+++ ceaser_cipher.py
@@ -12,7 +12,7 @@ def ceaser_cipher_encryption(plaintext, shift):
     encrypted_sentance = ''
     blank_space = ' '
     for letters in plaintext:
-        if plaintext != blank_space:
+        if letters != blank_space:
             index_of_letter = ALPHABET.index(letters)
             encrypted_index_of_letter = (index_of_letter + shift) % 26 
             letter_of_enctypted_value = ALPHABET[encrypted_index_of_letter]
@@ -20,9 +20,25 @@ def ceaser_cipher_encryption(plaintext, shift):
     return encrypted_sentance
 
 # TESTING - ceaser_cipher_encryption function
+#text_to_encrypt = 'A B C D'
+#encrypted_code = ceaser_cipher_encryption(text_to_encrypt, 1)
+#print(encrypted_code)
 
-# text_to_encrypt = 'ABCD'
-# encrypted_code = ceaser_cipher_encryption(text_to_encrypt, 2)
-# print(encrypted_code)
+def ceaser_cipher_decrypter(encrypted_text, shift):
+    '''decrypts an encrypted text when you know what shift is being used'''
+    deciphered_text = ''
+    blank_space = ' '
+    for letters in encrypted_text:
+        if letters != blank_space:
+            index_of_letter = ALPHABET.index(letters)
+            decrypted_index_of_letter_index = (index_of_letter - shift) % 26 
+            if decrypted_index_of_letter_index < 0:
+                decrypted_index_of_letter_index += 26 
+            letter_of_enctypted_value = ALPHABET[decrypted_index_of_letter_index]
+            deciphered_text += str(letter_of_enctypted_value) +  ' '
+    return deciphered_text
 
-
+# TESTING - ceaser_cipher_decryption function
+#encrypted_text = 'A B C D'
+#deciphered_text = ceaser_cipher_decrypter(encrypted_text, 2)
+#print(deciphered_text)
