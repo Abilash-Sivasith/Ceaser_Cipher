9f791c11422c94ff6a653bfca17f6b24e56162e9
Abi <abi.sivasith.world@gmail.com>
2023-12-18 18:04:31 +1300

    created and tested ceaser_cipher_decryption_guesser function'

--- ceaser_cipher.py
+++ ceaser_cipher.py
@@ -15,16 +15,16 @@ def ceaser_cipher_encryption(plaintext, shift):
     encrypted_sentance = ''
     for letters in plaintext:
         if letters != BLANK_SPACE:
-            index_of_letter = ALPHABET.index(letters)
+            index_of_letter = ALPHABET.index(letters.upper())
             encrypted_index_of_letter = (index_of_letter + shift) % 26 
             letter_of_enctypted_value = ALPHABET[encrypted_index_of_letter]
             encrypted_sentance += str(letter_of_enctypted_value) +  ' '
     return encrypted_sentance
 
 # TESTING - ceaser_cipher_encryption function
-#text_to_encrypt = 'A B C D'
-#encrypted_code = ceaser_cipher_encryption(text_to_encrypt, 1)
-#print(encrypted_code)
+text_to_encrypt = 'Hello World'
+encrypted_code = ceaser_cipher_encryption(text_to_encrypt, 8)
+print(encrypted_code)
 
 ######################################################################################
 
@@ -33,7 +33,7 @@ def ceaser_cipher_decrypter(encrypted_text, shift):
     deciphered_text = ''
     for letters in encrypted_text:
         if letters != BLANK_SPACE:
-            index_of_letter = ALPHABET.index(letters)
+            index_of_letter = ALPHABET.index(letters.upper())
             decrypted_index_of_letter_index = (index_of_letter - shift) % 26 
             if decrypted_index_of_letter_index < 0:
                 decrypted_index_of_letter_index += 26 
@@ -42,7 +42,7 @@ def ceaser_cipher_decrypter(encrypted_text, shift):
     return deciphered_text
 
 # TESTING - ceaser_cipher_decryption function
-#encrypted_text = 'A B C D'
+#encrypted_text = 'Hello World'
 #deciphered_text = ceaser_cipher_decrypter(encrypted_text, 2)
 #print(deciphered_text)
 
@@ -50,17 +50,23 @@ def ceaser_cipher_decrypter(encrypted_text, shift):
 
 def ceaser_cipher_decryption_guesser(cipher_text):
     '''allows the user to enter a number and see if the text revelled is decrpyted'''
-    user_input = input('Ceaser Shift to Try, enter ' ' to stop: ')
+    user_input = (input('Ceaser Shift to Try (enter a num between 0 and 25, inclusive): '))    
+    if user_input == 'terminate':
+        print('PROGRAM HAS FINNISHED EXACUTING')
+        print('This Could be Due to not entering a Valid Int\nOR because you have purpossfully closed the program')     
+        return 'Completted'
     user_input = int(user_input)
-    if user_input != int:
-        print('Please enter a number between 0 and 26')
+    if user_input < 26 and user_input >= 0:
+        print('trying a shift of ' + str(user_input))
+        print(ceaser_cipher_decrypter(cipher_text, user_input))
+        print('upto here')
         ceaser_cipher_decryption_guesser(cipher_text)
-    blank_space = ' '
-
-
-    while user_input != blank_space:
-        ceaser_cipher_decrypter(cipher_text, user_input)
-
-user_input = int(input('you moron'))
-if type(user_input) == int:
-    print('True')
+    elif user_input >= 26 or user_input < 0:
+        print('YOU MUST ENTER A VALUE BETWEEN 0 AND 26')
+        ceaser_cipher_decryption_guesser(cipher_text)
+    
+    
+# TESTING - ceaser_cipher_decryption_guesser function 
+# test_cipher = 'P M T T W E W Z T L'
+# test_decrpytion = ceaser_cipher_decryption_guesser(test_cipher)
+# print(test_decrpytion)
